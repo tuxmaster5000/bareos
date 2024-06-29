14f9f2e51c4ca9dbdd853c2e142a2aba8973ab56
tuxmaster5000 <837503+tuxmaster5000@users.noreply.github.com>
2024-06-29 18:18:54 +0200

    Creation of the hull

--- core/src/plugins/filed/zfs/zfs-fd.cc
+++ core/src/plugins/filed/zfs/zfs-fd.cc
@@ -15,16 +15,236 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <unistd.h>
+#include "zfs-fd.h"
 
-#include "include/fcntl_def.h"
-#include "include/bareos.h"
-#include "include/filetypes.h"
-#include "filed/fd_plugins.h"
-#include "plugins/include/common.h"
 
 /* ZFS header*/
+/*
 #include <libzfs.h>
+*/
 
 namespace filedaemon {
+
+
+/* Plugin Information block */
+static PluginInformation pluginInfo
+    = {sizeof(pluginInfo), FD_PLUGIN_INTERFACE_VERSION,
+       FD_PLUGIN_MAGIC,    PLUGIN_LICENSE,
+       PLUGIN_AUTHOR,      PLUGIN_DATE,
+       PLUGIN_VERSION,     PLUGIN_DESCRIPTION,
+       PLUGIN_USAGE};
+
+/* Plugin entry points for Bareos */
+static PluginFunctions pluginFuncs
+    = {sizeof(pluginFuncs), FD_PLUGIN_INTERFACE_VERSION,
+
+       /* Entry points into plugin */
+       newPlugin,  /* new plugin instance */
+       freePlugin, /* free plugin instance */
+       getPluginValue, setPluginValue, handlePluginEvent, startBackupFile,
+       endBackupFile, startRestoreFile, endRestoreFile, pluginIO, createFile,
+       setFileAttributes, checkFile, getAcl, setAcl, getXattr, setXattr};
+/**
+ * loadPlugin() and unloadPlugin() are called direct by the fd, so
+ * it must have an pure C signature.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* When the fd will load it. */
+bRC loadPlugin( PluginApiDefinition* libinfo,
+               CoreFunctions* lbareos_core_functions,
+               PluginInformation** plugin_information,
+               PluginFunctions** plugin_functions) {
+  bareos_core_functions = lbareos_core_functions; /* set Bareos funct pointers */
+  bareos_plugin_interface_version = libinfo;
+  *plugin_information = &pluginInfo; /* return pointer to our info */
+  *plugin_functions = &pluginFuncs;  /* return pointer to our functions */
+
+  return bRC_OK;
+}
+
+/* When the fd will unload it. */
+bRC unloadPlugin() {
+  return bRC_OK;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
+/* Create a new instance of the plugin i.e. allocate our private storage */
+static bRC newPlugin(PluginContext* ctx) {      
+  struct plugin_ctx* p_ctx = (struct plugin_ctx*)malloc(sizeof(struct plugin_ctx));
+  if (!p_ctx) {
+    return bRC_Error;
+  }
+  memset(p_ctx, 0, sizeof(struct plugin_ctx));
+  ctx->plugin_private_context = (void*)p_ctx; /* set our context pointer */
+
+  bareos_core_functions->registerBareosEvents(
+      ctx, 6, bEventNewPluginOptions, bEventPluginCommand, bEventJobStart,
+      bEventRestoreCommand, bEventEstimateCommand, bEventBackupCommand);
+
+  return bRC_OK;        
+} 
+// Free a plugin instance, i.e. release our private storage
+static bRC freePlugin(PluginContext* ctx) {      
+  struct plugin_ctx* p_ctx = (struct plugin_ctx*)ctx->plugin_private_context;
+       
+  if (!p_ctx) {
+    return bRC_Error;
+  }
+  if (p_ctx->plugin_options) {
+    free(p_ctx->plugin_options);
+  }  
+  free(p_ctx); /* free our private context */
+  p_ctx = nullptr;
+  return bRC_OK;
+}
+// Not used current in Bareos
+static bRC getPluginValue([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] pVariable var, [[maybe_unused]] void* value) {
+  return bRC_OK;
+}
+// Not used current in Bareos
+static bRC setPluginValue([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] pVariable var, [[maybe_unused]] void* value) {
+  return bRC_OK;
+}
+// Handle an event that comes from Bareos
+static bRC handlePluginEvent(PluginContext* ctx, bEvent* event, void* value) {
+  bRC retval = bRC_OK;
+  struct plugin_ctx* p_ctx = (struct plugin_ctx*)ctx->plugin_private_context;
+  // No plug-in context -> something must be wrong.
+  if (!p_ctx) {
+    return bRC_Error;
+  }
+  switch (event->eventType) {
+    case bEventJobStart:
+      Dmsg(ctx, debuglevel, "zfs-fd: JobStart=%s\n", (char*)value);
+      break;
+    case bEventRestoreCommand:
+      // Fall-through wanted
+    case bEventBackupCommand:
+      // Fall-through wanted
+    case bEventEstimateCommand:
+      // Fall-through wanted
+    case bEventPluginCommand:
+      /* must be done */
+      //retval = parse_plugin_definition(ctx, value);
+      break;
+    case bEventNewPluginOptions:
+      // Free any previous value.
+      if (p_ctx->plugin_options) {
+        free(p_ctx->plugin_options);
+        p_ctx->plugin_options = nullptr;
+      }
+      /* must be done */
+      //retval = parse_plugin_definition(ctx, value);
+      // Save that we got a plugin override.
+      p_ctx->plugin_options = strdup((char*)value);
+      break;
+    default:
+      Jmsg(ctx, M_FATAL, "zfs-fd: unknown event=%d\n", event->eventType);
+      Dmsg(ctx, debuglevel, "zfs-fd: unknown event=%d\n", event->eventType);
+      retval = bRC_Error;
+      break;
+  }
+  return retval; 
+}
+/* Start the backup of an file */
+static bRC startBackupFile(PluginContext* ctx, [[maybe_unused]] save_pkt* sp) {
+  if (plugin_has_all_arguments(ctx) != bRC_OK) {
+    return bRC_Error; 
+  }
+  //time_t now;
+  struct plugin_ctx* p_ctx;
+  p_ctx = (struct plugin_ctx*)ctx->plugin_private_context;
+  if (!p_ctx) {
+    return bRC_Error;
+  }
+  /* Not implemented */
+  return bRC_Error;
+}
+/* Done with backup of this file */
+static bRC endBackupFile([[maybe_unused]] PluginContext* ctx) {
+  /* bRC_OK when it was the last one
+   * bRC_More when more to backup*/
+  return bRC_OK;
+}
+/* When the restore starts */
+static bRC startRestoreFile(PluginContext* ctx, [[maybe_unused]] const char* cmd) {
+  if (plugin_has_all_arguments(ctx) != bRC_OK) {
+    return bRC_Error;
+  }
+  return bRC_OK;
+}
+/* When the restore ends*/
+static bRC endRestoreFile(PluginContext* ctx) {
+  struct plugin_ctx* p_ctx = (struct plugin_ctx*)ctx->plugin_private_context;
+  if (!p_ctx) {
+    return bRC_Error;
+  }
+  return bRC_OK;
+}
+/* Now the data I/O will be done */
+static bRC pluginIO(PluginContext* ctx, [[maybe_unused]] io_pkt* io) {
+  struct plugin_ctx* p_ctx = (struct plugin_ctx*)ctx->plugin_private_context;
+  if (!p_ctx) {
+    return bRC_Error;
+  }
+  /* Not impelented now */
+  return bRC_Error; 
+}
+/* On restore create the file before the data comes
+ * Here we must prepare the ZFS/zfs_revice stuff
+ * */
+static bRC createFile([[maybe_unused]] PluginContext* ctx, restore_pkt* rp) {
+  /* because not implemented yet */
+  rp->create_status = CF_SKIP;
+  return bRC_OK;
+}
+/* We don't need file attrubutes for ZFS, so ignore this. */
+static bRC setFileAttributes([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] restore_pkt* rp) {
+  return bRC_OK;
+}
+/* Check if an file exists in Inc/Diff backups, but this don't matter for zfs_send */
+static bRC checkFile([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] char* fname) {
+  return bRC_OK;
+}
+/* not needed for ZFS */
+static bRC getAcl([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] acl_pkt* ap) {
+  return bRC_OK;
+}
+/* not needed for ZFS */
+static bRC setAcl([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] acl_pkt* ap) {
+  return bRC_OK;
+}
+/* not needed for ZFS */
+static bRC getXattr([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] xattr_pkt* xp) {
+  return bRC_OK;
+}
+/* not needed for ZFS */
+static bRC setXattr([[maybe_unused]] PluginContext* ctx, [[maybe_unused]] xattr_pkt* xp) {
+  return bRC_OK;
+}
+
+/* Plug-In functions */
+static bRC plugin_has_all_arguments(PluginContext* ctx) 
+{
+  bRC retval = bRC_OK;
+  plugin_ctx* p_ctx = (plugin_ctx*)ctx->plugin_private_context;
+
+  if (!p_ctx) {
+    return bRC_Error;
+  }
+
+  /* Must be done */
+
+  return retval;
+}
+
+
 } /* namespace filedaemon */
---
+++ core/src/plugins/filed/zfs/zfs-fd.h
@@ -0,0 +1,85 @@
+/*
+   Copyright (C) 2024 tuxmaster5000 tuxmaster5000@googlemail.com
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Tuxmaster5000, Jun 2024
+
+/**
+ * @file
+ * The ZFS plugin for the Bareos File Daemon
+ */
+
+#ifndef ZFS_FD_HEADER
+#define ZFS_FD_HEADER 
+
+
+#include <unistd.h>
+
+#include "include/fcntl_def.h"
+#include "include/bareos.h"
+#include "include/filetypes.h"
+#include "filed/fd_plugins.h"
+#include "plugins/include/common.h"
+
+namespace filedaemon {
+
+#define PLUGIN_LICENSE "GPLv3+"
+#define PLUGIN_AUTHOR "Tuxmaster5000"
+#define PLUGIN_DATE ZFS_BUILD_STAMP
+#define PLUGIN_VERSION ZFS_BUILD_VERSION
+#define PLUGIN_DESCRIPTION "ZFS File Daemon Plugin"
+#define PLUGIN_USAGE                                                   \
+  "zfs:option=<setting>:option2=<setting>:option3=<setting>\n" \
+  " option foo\n" \
+  " option1 bar\n" \
+  " option2 bar\n" \
+  " option2 foo"
+
+/* Pointers to Bareos functions */
+static CoreFunctions* bareos_core_functions = nullptr;
+static PluginApiDefinition* bareos_plugin_interface_version = nullptr;
+
+
+/* Forward referenced functions */
+static bRC newPlugin(PluginContext* ctx);
+static bRC freePlugin(PluginContext* ctx);
+static bRC getPluginValue(PluginContext* ctx, pVariable var, void* value);
+static bRC setPluginValue(PluginContext* ctx, pVariable var, void* value);
+static bRC handlePluginEvent(PluginContext* ctx, bEvent* event, void* value);
+static bRC startBackupFile(PluginContext* ctx, save_pkt* sp);
+static bRC endBackupFile(PluginContext* ctx);
+static bRC startRestoreFile(PluginContext* ctx, const char* cmd);
+static bRC endRestoreFile(PluginContext* ctx);
+static bRC pluginIO(PluginContext* ctx, io_pkt* io);
+static bRC createFile(PluginContext* ctx, restore_pkt* rp);
+static bRC setFileAttributes(PluginContext* ctx, restore_pkt* rp);
+static bRC checkFile(PluginContext* ctx, char* fname);
+static bRC getAcl(PluginContext* ctx, acl_pkt* ap);
+static bRC setAcl(PluginContext* ctx, acl_pkt* ap);
+static bRC getXattr(PluginContext* ctx, xattr_pkt* xp);
+static bRC setXattr(PluginContext* ctx, xattr_pkt* xp);
+
+static bRC plugin_has_all_arguments(PluginContext* ctx);
+
+/* Plugin private context */
+struct plugin_ctx {
+  char* plugin_options;
+};
+
+static const int debuglevel = 150;
+
+} /* namespace filedaemon */
+#endif /* ZFS_FD_HEADER */
