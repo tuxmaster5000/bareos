51f142ad7cd1657c577b98dc9d0fbb7b680e9597
tuxmaster5000 <837503+tuxmaster5000@users.noreply.github.com>
2024-08-28 11:14:56 +0200

    Fix test

--- Meine Infos/test.py
+++ Meine Infos/test.py
@@ -1,4 +1,4 @@
 import bareos
 
-fd = bareos.bsock.filedaemon.FileDaemon(address="::1", port=8888, password=bareos.util.Password("demo"), name="dummy_dir", dirname="dummy_dir")
+fd = bareos.bsock.filedaemon.FileDaemon(address="::1", port=8888, password=bareos.util.Password("demo"), name="DIR-Test", dirname="DIR-Test")
 print(fd.call("status").decode("utf-8"))
