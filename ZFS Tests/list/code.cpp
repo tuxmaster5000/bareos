992c09e8014eee42e6177257312b30140d40c946
tuxmaster5000 <tuxmaster@users.noreply.github.com>
2024-11-24 15:21:41 +0100

    Umbau um dann die Elemente vom Pool zu lesen

--- ZFS Tests/list/code.cpp
+++ ZFS Tests/list/code.cpp
@@ -4,11 +4,42 @@
 #include <cstdlib>
 
 using namespace std;
-void cleanup(libzfs_handle_t* was) {
-	libzfs_fini(was);
+
+typedef struct {
+	uint* pool_counter;
+	libzfs_handle_t* libzfs;
+} data_struct;
+void cleanup(data_struct* was) {
+	libzfs_fini(was->libzfs);
 	printf("Aufgeräumt\n");
 }
-
+int pool_info(zpool_handle_t* pool, void* data) {
+	const char *p_name;
+	data_struct* my_data= static_cast<data_struct*>(data); 
+	p_name = zpool_get_name(pool);
+	zfs_handle_t* my_pool=zfs_open(my_data->libzfs, p_name,ZFS_TYPE_FILESYSTEM | ZFS_TYPE_VOLUME);
+	if(pool) {
+		printf("%s:\n", p_name);
+		++*my_data->pool_counter;
+		zfs_close(my_pool);
+	} else {
+		perror(p_name);
+		return 1;
+	}
+	return 0;
+}
+bool pool_list(data_struct* data) {
+	printf("Suche alle Pools\n");
+	uint* pool_count = new uint(0);
+	data->pool_counter = pool_count;
+	if(zpool_iter(data->libzfs, pool_info, data) != 0)
+	{
+		printf("Konnte die Pools nicht auswerten.\n%s\n", libzfs_error_description(data->libzfs));
+		return false;
+	}
+	printf("%i Pool(s) gefunden.\n", *pool_count);
+	return true;
+}
 int main() {
 	libzfs_handle_t* libzfs_hadle = libzfs_init();
 	if(libzfs_hadle)
@@ -17,5 +48,8 @@ int main() {
 		printf("ZFS konnte nich geladen werden.\n%s\n", libzfs_error_init(errno));
 		exit(1);
 	}
-	cleanup(libzfs_hadle);
+	data_struct data;
+	data.libzfs = libzfs_hadle;
+	pool_list(&data);
+	cleanup(&data);
 }
